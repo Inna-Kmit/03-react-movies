2197c40f51b35e492cc445f39370c943a3d9d0e1
Inna-Kmit <108284731+Inna-Kmit@users.noreply.github.com>
2025-12-22 05:04:38 -0800

    Create movieService.ts

---
+++ src/services/movieService.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { Movie } from "../types/movie";
+
+interface MovieHttpResponse {
+  readonly VITE_TMDB_TOKEN: string;
+  page: number;
+  results: Movie[];
+  total_pages: number;
+}
+const BASE_URL = "https://api.themoviedb.org/3";
+export const fetchMovies = async (query: string): Promise<Movie[]> => {
+  const MY_KEY = import.meta.env.VITE_TMDB_TOKEN as string | undefined;
+  const response = await axios.get<MovieHttpResponse>(
+    `${BASE_URL}/search/movie`,
+    {
+      params: {
+        query,
+        include_adult: false,
+        language: "en-US",
+        page: 1,
+      },
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${MY_KEY}`,
+      },
+    }
+  );
+  return response.data.results;
+};

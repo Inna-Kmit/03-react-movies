a9628d8fc942385e6e76df939e7e63f1cfc18549
Inna-Kmit <108284731+Inna-Kmit@users.noreply.github.com>
2025-12-22 06:32:02 -0800

    Update App.tsx

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
-import { fetchMovies } from "../../services/movieService";
-import { Movie } from "../../types/movie";
 import toast from "react-hot-toast";
+
+import { fetchMovies } from "../../services/movieService";
+import type { Movie } from "../../types/movie";
+
+import SearchBar from "../SearchBar/SearchBar";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import Loader from "../Loader/Loader";
 import MovieGrid from "../MovieGrid/MovieGrid";
-import SearchBar from "../SearchBar/SearchBar";
 import MovieModal from "../MovieModal/MovieModal";
+
 import css from "./App.module.css";
 
 export default function App() {
@@ -19,21 +22,24 @@ export default function App() {
     setMovies([]);
     setSelectedMovie(null);
     setIsError(false);
+
     try {
       setIsLoading(true);
       const data = await fetchMovies(query);
 
-      if (data.length) {
-        setMovies(data);
-      } else {
+      if (data.length === 0) {
         toast.error("No movies found for your request.");
+        return;
       }
+
+      setMovies(data);
     } catch {
       setIsError(true);
     } finally {
       setIsLoading(false);
     }
   };
+
   return (
     <div className={css.app}>
       <SearchBar onSubmit={handleSearch} />
@@ -44,14 +50,13 @@ export default function App() {
       {!isLoading && !isError && movies.length > 0 && (
         <MovieGrid movies={movies} onSelect={setSelectedMovie} />
       )}
+
       {selectedMovie && (
         <MovieModal
           movie={selectedMovie}
           onClose={() => setSelectedMovie(null)}
         />
       )}
-
-      <Toaster position="top-center" reverseOrder={false} />
     </div>
   );
 }
